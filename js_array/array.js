3a43901e61a34d40d65db7926f0a76f14c3619a8
Harshit Tyagi <tyagiharshit758@gmail.com>
2026-09-06 15:39:07 +0530

    new add

--- js_array/array.js
+++ js_array/array.js
@@ -15,25 +15,23 @@ myArr.push(6);
 myArr.pop();
 //console.log(myArr);
 
-//myArr.unshift(9);
-//console.log(myArr);
+myArr.unshift(9);
+//console.log(myArr);//push 9 at myArr[0] and shift every element to next index; 
 myArr.shift();
-//console.log(myArr);
+//console.log(myArr);//remove the element at myArr[0] and shift every element to its previous index;
 
-//console.log(myArr.includes(9));
-//console.log(myArr.indexOf(3));
+//console.log(myArr.includes(9));//check if 9 is present in myArr or not
+//console.log(myArr.indexOf(3));//give the index at which 3 is present
 
-const newArr = myArr.join();
-//console.log(myArr);
-//console.log(newArr);
+const newArr = myArr.join();//create a new string and transfer all elements of myArr to newArr ex 1,2,3,4
+//console.log(myArr);//[1,2,3,4]
+//console.log(newArr);//1,2,3,4
 
 //slice and splice PART 
-console.log("A",myArr);
-const myn1 = myArr.slice(1,3);
-console.log(myn1);
-console.log("B",myArr);
-const myn2 = myArr.splice(1,3);
-console.log(myn2);
-console.log("C",myArr);
-
-
+//console.log("A",myArr);// A [1,2,3,4] print 'A' before myArr
+const myn1 = myArr.slice(1,3); //take all elemnents from array other than from index 1 to 2 ans store them in myn1
+console.log(myn1);//[2,3]
+console.log("B",myArr);//B [1,2,3,4]
+const myn2 = myArr.splice(1,3);//remove all elemnts from array other than from index 1 to 3 ans store them in myn2
+console.log(myn2);//[2,3,4]
+console.log("C",myArr);//C[1]
--- js_array/array2.js
+++ js_array/array2.js
@@ -2,26 +2,28 @@ const arr1 = [1,2,3,4,5];
 const arr2 = [6,7,8,9,10];
 
 //arr1.push(arr2);
-//console.log(arr1);
+//console.log(arr1);//output : [ 1, 2, 3, 4, 5, [ 6, 7, 8, 9, 10 ] ]
 
-const arr3 = arr1.concat(arr2);
-//console.log(arr3);
+const arr3 = arr1.concat(arr2);//concat method
+//console.log(arr3);//output: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const arr4 = [...arr1,...arr2,...arr3];
-//console.log(arr4);
+const arr4 = [0,...arr1,2,...arr2,3,...arr3];//spread operator
+//console.log(arr4);//output: [ 0, 1, 2, 3, 4, 5, 2, 6, 7, 8, 9, 10, 3, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const arr5 = [1,2,3,[4,5],7,[8,9,[10,11]]];
-const arr6 = arr5.flat(Infinity);
+const arr6 = arr5.flat(1);
+const arr7 = arr5.flat(Infinity); //output: [ 1, 2, 3, 4, 5, 7, 8, 9, 10, 11] flat operator is used flatten a nested array and it takes depth as arguement here infinty means flatten all levels
 //console.log(arr5);
-//console.log(arr6);
+//console.log(arr6); // output : [ 1, 2, 3, 4, 5, 7, 8, 9, [ 10, 11 ] ]
+//console.log(arr7);
 
-console.log(Array.isArray("Harshit"))
-console.log(Array.from("Harshit"))
-console.log(Array.from({ name : "Harshit"} )) // IMPORTANT
+//console.log(Array.isArray("Harshit"))
+//console.log(Array.from("Harshit")) // otput : ['H', 'a', 'r', 's', 'h', 'i', 't']
+//console.log(Array.from({ name : "Harshit"} ))//object is not iterable and doesn't have a length property that Array.from() can use. // IMPORTANT
 
 
 let x = 10;
 let y = 20;
 let z = 30;
 
-console.log(Array.of(x,y,z));
+console.log(Array.of(x,y,z)); // [ 10, 20, 30 ]
